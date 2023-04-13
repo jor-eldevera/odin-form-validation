b584adc292ffb7ca444f0bd5c9951a421ab6fedf
jor-eldevera <jorell.devera@gmail.com>
2023-04-12 19:54:46 -0700

    Add event listener to password confirmation

--- scripts.js
+++ scripts.js
@@ -6,10 +6,17 @@ const password = document.getElementById("password");
 const passwordConfirm = document.getElementById("passwordConfirm");
 
 email.addEventListener("input", e => {
-    console.log("test");
     if (email.validity.typeMismatch) {
         email.setCustomValidity("Please enter an email address.")
     } else {
         email.setCustomValidity("");
+    }
+});
+
+passwordConfirm.addEventListener("input", e => {
+    if (passwordConfirm.value != password.value) {
+        passwordConfirm.setCustomValidity("Passwords must match.");
+    } else {
+        passwordConfirm.setCustomValidity("");
     }
 });
